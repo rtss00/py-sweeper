38832306d3a8f5170b06e58d317143e10da4f01a
rtss00 <r.tissoni@gmail.com>
2018-07-17 15:18:33 -0300

    Fixes

--- field_class.py
+++ field_class.py
@@ -15,6 +15,7 @@ class MinedField:
             else:
                 elem = Element('EMPTY')
             self.field.append(elem)
+        self.calculate_numbers()
 
     def translate_position(self, x: int, y: int) -> int:
         if (x <= self.side) and (y <= self.height):
@@ -35,23 +36,23 @@ class MinedField:
                 print('[{}]'.format(sym), end='')
             print()
 
-    def check_type(self,x: int, y: int) -> str:
+    def check_type(self, x: int, y: int) -> str:
         if (0 <= y <= self.height-1) and (0 <= x <= self.side):
             return self.field[self.translate_position(x, y)].type
         else:
             return 'EMPTY'
 
-    def count_bombs(self,x: int, y: int) -> int:
+    def count_bombs(self, x: int, y: int) -> int:
         count = 0;
-        for i in range(-1,2):
-            for j in range(-1,2):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
                 if self.check_type(x+i, y+j) == 'BOMB':
                     count += 1;
         return count
 
     def calculate_numbers(self):
-        for y in range(1, self.height):
-            for x in range(1, self.side):
+        for y in range(1, self.height+1):
+            for x in range(1, self.side+1):
                 point = self.field[self.translate_position(x, y)]
                 number = self.count_bombs(x, y)
                 if (point.type != 'BOMB') and (number > 0):
--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 from field_class import MinedField;
 
 
-mf = MinedField(5, 10, 12)
+mf = MinedField(10, 10, 12)
 mf.print_field()
+
 print("\nMines in positions:",end=' ')
 mf.mines.sort()
 for i in mf.mines:
