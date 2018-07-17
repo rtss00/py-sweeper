e3c800f3fa6681078f833cd04e97a02b9824d4ab
rtss00 <r.tissoni@gmail.com>
2018-07-17 16:24:32 -0300

    Colors added. Number calclulations need fixing in the last row.

--- field_class.py
+++ field_class.py
@@ -1,8 +1,20 @@
 import func
 from element_class import Element
+from sty import fg, bg, ef, rs
 
 
 class MinedField:
+    color_dic = {
+        1: 21,
+        2: 2,
+        3: 1,
+        4: 19,
+        5: 52,
+        6: 14,
+        7: 5,
+        8: 11
+    }
+
     def __init__(self, a, b, amount):
         self.side = a
         self.height = b
@@ -26,28 +38,30 @@ class MinedField:
     def print_field(self):
         for y in range(1, self.height+1):
             for x in range(1, self.side+1):
-                point = self.field[self.translate_position(x,y)]
+                point = self.field[self.translate_position(x, y)]
                 if point.type == 'EMPTY':
                     sym = ' '
                 elif point.type == 'NUMBER':
-                    sym = 'n'
+                    sym = str(point.number)
                 else:
                     sym = 'X'
-                print('[{}]'.format(sym), end='')
+                text = '[' + fg(self.color_dic[int(sym)]) + sym + fg.rs + ']' if point.type == 'NUMBER' \
+                    else '[{}]'.format(sym)
+                print(text, end='')
             print()
 
     def check_type(self, x: int, y: int) -> str:
-        if (0 <= y <= self.height-1) and (0 <= x <= self.side):
+        if (1 <= y <= self.height-1) and (1 <= x <= self.side):
             return self.field[self.translate_position(x, y)].type
         else:
             return 'EMPTY'
 
     def count_bombs(self, x: int, y: int) -> int:
-        count = 0;
+        count = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
                 if self.check_type(x+i, y+j) == 'BOMB':
-                    count += 1;
+                    count += 1
         return count
 
     def calculate_numbers(self):
--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 from field_class import MinedField;
+from sty import fg, bg, ef, rs
 
 
 mf = MinedField(10, 10, 12)
 mf.print_field()
 
-print("\nMines in positions:",end=' ')
+print(fg(34) + "\nMines in positions:" + fg.rs, end=' ')
 mf.mines.sort()
 for i in mf.mines:
     print(i, end=' ')
