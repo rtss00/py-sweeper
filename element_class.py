5bd41f667982c9a32294a3fd78533e97a6eaf8a7
rtss00 <r.tissoni@gmail.com>
2018-07-19 10:16:17 -0300

    Colors added to elemement_class

--- element_class.py
+++ element_class.py
@@ -1,5 +1,18 @@
+from sty import fg
+
+
 class Element:
     types = ('BOMB', 'EMPTY', 'NUMBER')
+    color_dic = {
+        1: 21,
+        2: 2,
+        3: 1,
+        4: 19,
+        5: 52,
+        6: 14,
+        7: 5,
+        8: 11
+    }
 
     def __init__(self, elem_type):
         if elem_type in self.types:
@@ -25,7 +38,7 @@ class Element:
             self.graphic = 'X'
         elif _type == 'NUMBER':
             self.type = 'NUMBER'
-            self.graphic = str(number)
+            self.graphic = fg(self.color_dic[number]) + str(number) + fg.rs
         else:
             self.type = 'EMPTY'
             self.graphic = ' '
