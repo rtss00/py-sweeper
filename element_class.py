04c6e5b35a61c7acfa2fa29c706dad1f64f22da0
rtss00 <r.tissoni@gmail.com>
2018-08-06 18:09:27 -0300

    Element Class refactored to Cell, and fully commented.

--- element_class.py
+++ element_class.py
@@ -2,19 +2,17 @@
 
 
 class Cell:
+    # Only valid cell types
     types = ('BOMB', 'EMPTY', 'NUMBER')
-    color_dic = {
-        1: 21,
-        2: 2,
-        3: 1,
-        4: 19,
-        5: 52,
-        6: 14,
-        7: 5,
-        8: 11
-    }
+    # Color dictionary for previous versions
+    # color_dic = {1: 21, 2: 2, 3: 1, 4: 19, 5: 52, 6: 14, 7: 5, 8: 11}
 
-    def __init__(self, elem_type):
+    def __init__(self, elem_type=None):
+        """
+        Initialize a Cell object, un-flagged, un-opened, with number 0 by default, with the given type.
+        If no argument is passed, or the argument is not in Cell.types, The type will be set as 'EMPTY'.
+        :param elem_type: Type of cell. Possible values: 'BOMB', 'NUMBER', 'EMPTY'
+        """
         if elem_type in self.types:
             self.type = elem_type
         else:
@@ -25,27 +23,34 @@ class Cell:
         self.graphic = ' ' if self.type == 'EMPTY' else 'X'
 
     def flag(self):
+        """
+        Flag cell.
+        :return:
+        """
         # only flag if it's un-flagged or not open.
         self.flagged = True if (not self.flagged) and (not self.opened) else False
 
     def open(self):
+        """
+        Open cell. Propagation not available at cell level, only at field level.
+        :return: None
+        """
         # only open if it's not flagged
         self.opened = True if not self.flagged else False
 
     def change_type(self, _type, number=0):
-        if _type == 'BOMB':
-            self.type = 'BOMB'
-            self.graphic = 'X'
-            self.number = number
-        elif _type == 'NUMBER':
-            self.type = 'NUMBER'
-            # Color for previous versions
-            # self.graphic = fg(self.color_dic[number]) + str(number) + fg.rs
-            self.graphic = str(number)
-            self.number = number
-        else:
-            self.type = 'EMPTY'
+        """
+        Changes the type of the cell to [_type]. If type wrong type is provided, the function has no effect.
+        If [_type]=='NUMBER' the number on the cell will be changed to the passed argument. Otherwise it'll be set to 0
+        :param _type: Type to change. Must be in Cell.types
+        :param number: Value to set if [_type]=='NUMBER' (1 <= number <= 8 to maintain consistency). Default to 0.
+        :return: None
+        """
+        if _type in self.types:
+            self.type = _type
             self.graphic = ' '
-            self.number = number
+            self.graphic = str(number) if _type == 'NUMBER' else 'X'
+            self.number = number if _type == 'NUMBER' else 0
+
 
 
